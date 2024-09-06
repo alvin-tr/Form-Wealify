aa92cfec1cbfdd0ec69dbf914041eaa0d0069b08
alvintran888 <106613854+alvintran888@users.noreply.github.com>
2024-09-06 17:38:11 +0700

    gehp API

--- js/bankWealify.js
+++ js/bankWealify.js
@@ -1,4 +1,4 @@
-const contentContainer = document.querySelector('.content_container');
+
 
 
 
@@ -21,7 +21,6 @@ function clickEvenCard() {
 }
 
 
-
 fetch('https://dev-api.wealify.com/api/v1/cms/constants/providers', {
     method: 'GET',
     headers: {
@@ -35,46 +34,24 @@ fetch('https://dev-api.wealify.com/api/v1/cms/constants/providers', {
     .then(({ data }) => {
         console.log('NCC:', data);
         providerList(data)
+        console.log(' providerList(data)', providerList(data));
 
     })
     .catch((error) => {
         console.error('Lỗi khi gọi dữ liệu:', error);
     })
 
+let isProviderListCalled = false;
+async function providerList(NCCS) {
+    if (isProviderListCalled) return;
+    isProviderListCalled = true;
+    const contentContainer = document.querySelector('.content_container');
+    contentContainer.innerHTML = '';
 
-async function fetchListCard() {
-    try {
-        const response = await fetch('https://dev-api.wealify.com/api/v1/cms/system-payments', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            },
-            params: {
-                page: 1,
-                limit: 1000
-            }
-        });
-
-        const { data } = await response.json();
-
-        console.log('data Await:', data);
-
-        cardContainer(data)
-        console.log('cardContainer(data)', cardContainer(data));
-
-
-
-    } catch (error) {
-        console.log('Khong the doc du lieu: ', error);
-    }
-}
-
-
-
-function providerList(NCCS) {
     const container = document.createElement("div");
     container.className = "dropdown_element";
+
+    let cardRender = await cardContainer();
 
     container.innerHTML = NCCS.map(ncc => {
 
@@ -104,7 +81,8 @@ function providerList(NCCS) {
 
                     <div class="card__item__container--grand">
                     
-                        ${cardContainer()}
+                    ${cardRender}
+                        
                     </div>
 
                     </div>
@@ -122,23 +100,65 @@ function providerList(NCCS) {
     contentContainer.appendChild(container)
     clickEvenCard()
 
-
-    cardContainer(cardListItem)
-
 }
 
-function cardContainer() {
+
+// async function fetchListCard() {
+//     try {
+//         const response = await fetch('https://dev-api.wealify.com/api/v1/cms/system-payments', {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+//             },
+//             params: {
+//                 page: 1,
+//                 limit: 1000
+//             }
+//         });
+
+//         const { data } = await response.json();
+
+//         console.log('data Await:', data);
+
+//         // cardContainer(data)
+
+
+//     } catch (error) {
+//         console.log('Khong the doc du lieu: ', error);
+//     }
+// }
 
 
 
+async function cardContainer() {
     let card = []
 
-    let cardRender = cards.map(card => {
-        return `
+    try {
+        const response = await fetch('https://dev-api.wealify.com/api/v1/cms/system-payments', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        const result = await response.json();
+        card = result.data;
+        console.log('cardokenh: ', card);
+
+        let cardRender = card.map(card => {
+
+            if (card.detail.account_name && card.detail.bank_name) {
+                return `
              <div class="card__item__container">
                                 <div class="card__item__header">
                                     <div class="content--header">
-                                        <h3>ACB - THE HUMAN BANK</h3>
+                                        <div class="h3__bank">
+                                        <h3>${card.detail.bank_name} </h3>&nbsp;&nbsp;
+                                        <h3> - </h3>&nbsp;&nbsp;
+                                        <h3>${card.detail.account_name}</h3>
+                                        </div>
+                                        
                                         <div class="uutien">
                                             <p>Mức độ ưu tiên</p>
                                             <span class="uutienNumber">1</span>
@@ -232,9 +252,113 @@ function cardContainer() {
                                 </div>
                             </div>
         `
-    }).join('')
+            } else if (card.detail.account_name) {
+                return `
+                 <div class="card__item__container">
+                                <div class="card__item__header">
+                                    <div class="content--header">
+                                        <h3>${card.detail.account_name} </h3>
+                                        <div class="uutien">
+                                            <p>Mức độ ưu tiên</p>
+                                            <span class="uutienNumber">1</span>
+                                        </div>
+                                    </div>
 
-    return cardRender;
+                                    <div class="content__button--header">
+                                        <!-- eye -->
+                                        <div class="hover__simple">
+                                            <i class="far fa-eye"></i>
+                                        </div>
+                                        <!-- pencil -->
+                                        <div class="hover__simple">
+                                            <i class="fad fa-pencil"></i>
+                                        </div>
+                                        <!-- trash -->
+                                        <div class="hover__simple">
+                                            <i class="fas fa-trash"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card__item__body">
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Ewallet
+                                        </p>
+                                        <p>ACB</p>
+                                    </div>
+                                    <!-- 2 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Email
+                                        </p>
+                                        <p>THE HUMAN BANK</p>
+                                    </div>
+                                    <!-- 3 -->
+                                    
+                                    <!-- 4 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Gán cho level
+                                        </p>
+                                        <p>Level 2</p>
+                                    </div>
+                                    <!-- 5 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Hạn mức/ngày
+                                        </p>
+                                        <p>12,312,213 VND</p>
+                                    </div>
+                                    <!-- 6 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Đã top-up/ngày
+                                        </p>
+                                        <p>0 VND</p>
+                                    </div>
+                                    <!-- 7 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Còn lại
+                                        </p>
+                                        <p>12,312,213 VND</p>
+                                    </div>
+                                    <!-- 8 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Trạng thái
+                                        </p>
+                                        <span>Còn hạn mức</span>
+                                    </div>
+                                    <!-- 9 -->
+                                    <div class="horizontal__item">
+                                        <p>
+                                            Active
+                                        </p>
+                                        <label class="switch">
+                                            <input type="checkbox" checked>
+                                            <span class="slider round" style="height: 34px;"></span>
+
+                                        </label>
+                                    </div>
+
+                                </div>
+                            </div>
+                `
+            }
+
+
+        }).join('')
+
+        return cardRender;
+    }
+    catch (error) {
+        console.log('Can not render data:', error);
+        return '';
+    }
+
+
 }
 
 
